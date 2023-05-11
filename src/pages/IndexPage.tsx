7c94ad0b3f6b5f136a1c36d98d2eb44af7f8cdc3
keyxcode <keyxcode@gmail.com>
2023-05-11 16:34:00 +0700

    Fill in section 2 content

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -3,58 +3,97 @@ import { Typography, Box, Grid, Container, Button, Stack } from '@mui/material';
 const SectionOne = () => (
     <>
         <Box component="section" py={6}>
-            <Grid container rowSpacing={{ xs: 5, md: 0 }} textAlign={{ xs: "center", md: "left" }} alignItems="center">
-                <Grid item xs={12} md={6} order={{ xs: 2, md: 1 }}>
-                    <Typography variant="h3" color="primary" sx={{ fontWeight: "bold", mb: 1 }}>
-                        Write. Learn. Grow.
-                    </Typography>
-                    <Typography variant="h6" paragraph sx={{ mb: 2 }}>
-                        Master grammar, spelling, and syntax in the language(s) you’re learning through direct feedback on your writing from fluent, native speakers.
-                    </Typography>
-                    <Button variant="contained" color="primary">
-                        Start learning ➔
-                    </Button>
-                    <Button color="primary">
-                        Browse as guest
-                    </Button>
+            <Container>
+                <Grid container rowSpacing={{ xs: 5, md: 0 }} textAlign={{ xs: "center", md: "left" }} alignItems="center">
+                    <Grid item xs={12} md={6} order={{ xs: 2, md: 1 }}>
+                        <Typography variant="h3" color="primary" sx={{ fontWeight: "bold", mb: 1 }}>
+                            Write. Learn. Grow.
+                        </Typography>
+                        <Typography variant="h6" paragraph sx={{ mb: 2 }}>
+                            Master grammar, spelling, and syntax in the language(s) you’re learning through direct feedback on your writing from fluent, native speakers.
+                        </Typography>
+                        <Button variant="contained" color="primary">
+                            Start learning ➔
+                        </Button>
+                        <Button color="primary">
+                            Browse as guest
+                        </Button>
+                    </Grid>
+                    <Grid item xs={12} md={6} order={{ xs: 1, md: 2 }}>
+                        <Box
+                            component="img"
+                            src="src/assets/index/image-index.svg"
+                            alt="img-index"
+                            sx={{ width: { xs: "80%", md: "100%" }, height: "auto" }}
+                        />
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={6} order={{ xs: 1, md: 2 }}>
-                    <Box
-                        component="img"
-                        src="src/assets/index/image-index.svg"
-                        alt="img-index"
-                        sx={{ width: { xs: "80%", md: "100%" }, height: "auto" }}
-                    />
-                </Grid>
-            </Grid>
+            </Container>
         </Box >
     </>
 )
 
 const SectionTwo = () => (
     <>
-        <Box component="section" py={6}>
-            <Stack>
-                <Grid container alignItems="center" justifyContent="center">
-                    <Grid item xs={12} md={8}>Getting corrections on your writing is really easy</Grid>
-                    <Grid item xs={12} md={4}>I'm an image</Grid>
-                </Grid>
-                <Grid container alignItems="center" justifyContent="center">
-                    <Grid item xs={12} md={4}>Register an account</Grid>
-                    <Grid item xs={12} md={4}>Write a journal entry</Grid>
-                    <Grid item xs={12} md={4}>Review corrections</Grid>
-                </Grid>
-            </Stack>
+        <Box component="section" py={6} bgcolor="primary.main" color="white">
+            <Container>
+                <Stack>
+                    <Grid container justifyContent="center">
+                        <Grid item xs={12} md={8}>
+                            <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
+                                Getting corrections on your writing is really easy
+                            </Typography>
+                            <Typography variant="h6" paragraph>
+                                Once you're done writing in your studying language, we will automatically match it with native speakers.
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} md={4}>
+                            <Box
+                                component="img"
+                                src="src/assets/index/illustration-1.webp"
+                                alt="illustration-1"
+                                sx={{ width: { xs: "50%", md: "80%" }, height: "auto" }}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Grid container justifyContent="center">
+                        <Grid item xs={12} md={4}>
+                            <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
+                                1 Register an account
+                            </Typography>
+                            <Typography variant="h6" paragraph>
+                                We start with a short 3-step registration process to help us determine users you should be match with.
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} md={4}>
+                            <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
+                                2 Write a journal entry
+                            </Typography>
+                            <Typography variant="h6" paragraph>
+                                Browse a wide variety of writing prompts or simply create a journal from scratch.
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} md={4}>
+                            <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
+                                3 Review corrections
+                            </Typography>
+                            <Typography variant="h6" paragraph>
+                                Native speakers will correct your writing and provide constructive feedback.
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Stack>
+            </Container>
         </Box>
     </>
 )
 
 const IndexPage = () => {
     return (
-        <Container>
+        <Box>
             <SectionOne />
             <SectionTwo />
-        </Container >
+        </Box >
     );
 }
 
