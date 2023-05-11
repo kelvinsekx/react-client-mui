967557cf44951d575b6659219b5ce9ecf3118b96
keyxcode <keyxcode@gmail.com>
2023-05-11 13:24:33 +0700

    Create index route for development

--- src/RoutesList.tsx
+++ src/RoutesList.tsx
@@ -2,6 +2,7 @@ import { Navigate, useRoutes } from 'react-router-dom';
 import DashboardLayout from './layouts/dashboard/DashboardLayout';
 import PostPage from "./pages/PostPage.tsx";
 import DashboardHomePage from "./pages/DashboardHomePage.tsx";
+import IndexPage from './pages/IndexPage.tsx';
 
 const RoutesList = () => {
     return useRoutes([
@@ -13,6 +14,10 @@ const RoutesList = () => {
                 { path: "teach", element: <PostPage /> },
                 { path: "learn", element: <PostPage /> },
             ]
+        },
+        {
+            path: "/index",
+            element: <IndexPage />
         },
         {
             path: '*',
