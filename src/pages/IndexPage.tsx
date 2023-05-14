f45cba82739ab086eb740b6b6c082089ed7d63a7
keyxcode <keyxcode@gmail.com>
2023-05-14 13:34:37 +0700

    Reduce repeated code in section 2 and 3

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -33,123 +33,124 @@ const SectionOne = () => (
     </>
 )
 
-const SectionTwo = () => (
-    <>
-        <Box component="section" py={6} bgcolor="primary.main" color="white">
-            <Container>
-                <Stack>
-                    <Grid container justifyContent="center" sx={{ mb: 5 }}>
-                        <Grid item xs={12} md={8} order={{ xs: 2, md: 1 }} sx={{ pr: 10 }}>
-                            <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
-                                Getting corrections on your writing is really easy
-                            </Typography>
-                            <Typography variant="h6" paragraph>
-                                Once you're done writing in your studying language, we will automatically match it with native speakers.
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12} md={4} order={{ xs: 1, md: 2 }} textAlign="center">
-                            <Box
-                                component="img"
-                                src="src/assets/index/illustration-1.webp"
-                                alt="illustration-1"
-                                sx={{ width: { xs: "50%", md: "80%" }, height: "auto" }}
-                            />
-                        </Grid>
-                    </Grid>
-                    <Grid container justifyContent="center">
-                        <Grid item xs={12} md={4}>
-                            <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
-                                1 Register an account
-                            </Typography>
-                            <Typography variant="h6" paragraph>
-                                We start with a short 3-step registration process to help us determine users you should be match with.
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12} md={4}>
-                            <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
-                                2 Write a journal entry
-                            </Typography>
-                            <Typography variant="h6" paragraph>
-                                Browse a wide variety of writing prompts or simply create a journal from scratch.
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12} md={4}>
-                            <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
-                                3 Review corrections
-                            </Typography>
-                            <Typography variant="h6" paragraph>
-                                Native speakers will correct your writing and provide constructive feedback.
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                </Stack>
-            </Container>
-        </Box>
-    </>
-)
+const SectionTwo = () => {
+    const grids = [
+        {
+            id: 1,
+            header: "1 Register an account",
+            content: "We start with a short 3-step registration process to help us determine users you should be match with."
+        },
+        {
+            id: 2,
+            header: "2 Write a journal entry",
+            content: "Browse a wide variety of writing prompts or simply create a journal from scratch."
+        },
+        {
+            id: 3,
+            header: "3 Review corrections",
+            content: "Native speakers will correct your writing and provide constructive feedback."
+        },
+    ]
 
-const SectionThree = () => (
-    <>
-        <Box component="section" py={6} >
-            <Container>
-                <Stack>
-                    <Grid container justifyContent="center">
-                        <Grid item xs={12} md={4}>
-                            <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
-                                Built for serious learners
-                            </Typography>
-                            <Typography variant="h6" paragraph>
-                                Ditch all the unnecessary distractions on other language-learning platforms and spend more time focusing on your new language(s).
-                            </Typography>
+    return (
+        <>
+            <Box component="section" py={6} bgcolor="primary.main" color="white">
+                <Container>
+                    <Stack>
+                        <Grid container justifyContent="center" sx={{ mb: 5 }}>
+                            <Grid item xs={12} md={8} order={{ xs: 2, md: 1 }} sx={{ pr: 10 }}>
+                                <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
+                                    Getting corrections on your writing is really easy
+                                </Typography>
+                                <Typography variant="h6" paragraph>
+                                    Once you're done writing in your studying language, we will automatically match it with native speakers.
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12} md={4} order={{ xs: 1, md: 2 }} textAlign="center">
+                                <Box
+                                    component="img"
+                                    src="src/assets/index/illustration-1.webp"
+                                    alt="illustration-1"
+                                    sx={{ width: { xs: "50%", md: "80%" }, height: "auto" }}
+                                />
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12} md={4}>
-                            <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
-                                Straight to the point
-                            </Typography>
-                            <Typography variant="h6" paragraph>
-                                Get corrections on your writing quickly – Simply write a journal in your target language, publish it, and let the LangCorrect community do their thing to provide corrections for you.
-                            </Typography>
+                        <Grid container justifyContent="center">
+                            {grids.map(grid => (
+                                <Grid key={grid.id} item xs={12} md={4}>
+                                    <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
+                                        {grid.header}
+                                    </Typography>
+                                    <Typography variant="h6" paragraph>
+                                        {grid.content}
+                                    </Typography>
+                                </Grid>
+                            ))}
                         </Grid>
-                        <Grid item xs={12} md={4}>
-                            <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
-                                Constantly improving
-                            </Typography>
-                            <Typography variant="h6" paragraph>
-                                The LangCorrect platform is getting better all the time. Consistent, direct feedback from our user base and frequent updates allow us to keep things fresh and interesting for our users.
-                            </Typography>
+                    </Stack>
+                </Container>
+            </Box>
+        </>
+    )
+}
+
+const SectionThree = () => {
+    const grids = [
+        {
+            id: 1,
+            header: "Built for serious learners",
+            content: "Ditch all the unnecessary distractions on other language-learning platforms and spend more time focusing on your new language(s)."
+        },
+        {
+            id: 2,
+            header: "Straight to the point",
+            content: "Get corrections on your writing quickly – Simply write a journal in your target language, publish it, and let the LangCorrect community do their thing to provide corrections for you."
+        },
+        {
+            id: 3,
+            header: "Constantly improving",
+            content: "The LangCorrect platform is getting better all the time. Consistent, direct feedback from our user base and frequent updates allow us to keep things fresh and interesting for our users."
+        },
+        {
+            id: 4,
+            header: "Learn with friends",
+            content: "Invite your friends to join LangCorrect and challenge one another to see who can earn the highest Rankings and Streaks."
+        },
+        {
+            id: 5,
+            header: "Functionality with learning in mind",
+            content: "From writing prompts to automatic, color-coded correction highlighting, learning to write in another language has never been this easy."
+        },
+        {
+            id: 6,
+            header: "Built-in messaging",
+            content: "Take a break to relax and chat with other learners using the messaging feature. This is a great way to make new friends and meet people from around the globe."
+        },
+    ]
+
+    return (
+        <>
+            <Box component="section" py={6} >
+                <Container>
+                    <Stack>
+                        <Grid container justifyContent="center">
+                            {grids.map(grid => (
+                                <Grid key={grid.id} item xs={12} md={4}>
+                                    <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
+                                        {grid.header}
+                                    </Typography>
+                                    <Typography variant="h6" paragraph>
+                                        {grid.content}
+                                    </Typography>
+                                </Grid>
+                            ))}
                         </Grid>
-                    </Grid>
-                    <Grid container justifyContent="center">
-                        <Grid item xs={12} md={4}>
-                            <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
-                                Learn with friends
-                            </Typography>
-                            <Typography variant="h6" paragraph>
-                                Invite your friends to join LangCorrect and challenge one another to see who can earn the highest Rankings and Streaks.
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12} md={4}>
-                            <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
-                                Functionality with learning in mind
-                            </Typography>
-                            <Typography variant="h6" paragraph>
-                                From writing prompts to automatic, color-coded correction highlighting, learning to write in another language has never been this easy.
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12} md={4}>
-                            <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
-                                Built-in messaging
-                            </Typography>
-                            <Typography variant="h6" paragraph>
-                                Take a break to relax and chat with other learners using the messaging feature. This is a great way to make new friends and meet people from around the globe.
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                </Stack>
-            </Container>
-        </Box>
-    </>
-)
+                    </Stack>
+                </Container>
+            </Box>
+        </>
+    )
+}
 
 const SectionFour = () => (
     <>
