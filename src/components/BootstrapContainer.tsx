8f0c98add08c85bb21b46a8a567d7d0212c759a9
keyxcode <keyxcode@gmail.com>
2023-05-16 09:22:57 +0700

    Refactor BootstrapContainer

--- src/components/BootstrapContainer.tsx
+++ src/components/BootstrapContainer.tsx
@@ -1,27 +1,25 @@
 import { styled } from '@mui/system';
 
-const BootstrapContainer = styled('div')(({ theme }) =>
-    theme.unstable_sx({
-        margin: "auto",
-        '@media (min-width: 0px)': {
-            maxWidth: "100%"
-        },
-        '@media (min-width: 576px)': {
-            maxWidth: 540
-        },
-        '@media (min-width: 768px)': {
-            maxWidth: 720
-        },
-        '@media (min-width: 992px)': {
-            maxWidth: 960
-        },
-        '@media (min-width: 1200px)': {
-            maxWidth: 1140
-        },
-        '@media (min-width: 1400px)': {
-            maxWidth: 1320
-        },
-    }),
-);
+const BootstrapContainer = styled("div")({
+    margin: "auto",
+    '@media (min-width: 0px)': {
+        maxWidth: "100%"
+    },
+    '@media (min-width: 576px)': {
+        maxWidth: 540
+    },
+    '@media (min-width: 768px)': {
+        maxWidth: 720
+    },
+    '@media (min-width: 992px)': {
+        maxWidth: 960
+    },
+    '@media (min-width: 1200px)': {
+        maxWidth: 1140
+    },
+    '@media (min-width: 1400px)': {
+        maxWidth: 1320
+    },
+});
 
 export default BootstrapContainer
