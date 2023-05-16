7847dc7c3c9fe59c71152add923821b21e61d361
keyxcode <keyxcode@gmail.com>
2023-05-16 07:34:32 +0700

    Improve sections text and alignment

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -4,7 +4,7 @@ import BootstrapContainer from '../components/BootstrapContainer';
 const SectionOne = () => (
     <Box component="section" py={6}>
         <BootstrapContainer>
-            <Grid container rowSpacing={{ xs: 5, md: 0 }} textAlign={{ xs: "center", md: "left" }} alignItems="center">
+            <Grid container textAlign={{ xs: "center", md: "left" }} alignItems="center">
                 <Grid item xs={12} md={6} order={{ xs: 2, md: 1 }}>
                     <Typography variant="h4" color="primary" sx={{ fontWeight: "bold", mb: 1 }}>
                         Write. Learn. Grow.
@@ -31,7 +31,7 @@ const SectionOne = () => (
                         component="img"
                         src="src/assets/index/image-index.svg"
                         alt="img-index"
-                        sx={{ width: { xs: "80%", md: "100%" }, height: "auto" }}
+                        sx={{ width: "100%", height: "auto" }}
                     />
                 </Grid>
             </Grid>
@@ -62,9 +62,9 @@ const SectionTwo = () => {
         <Box component="section" py={6} bgcolor="primary.main" color="white">
             <BootstrapContainer>
                 <Stack>
-                    <Grid container justifyContent="center" sx={{ mb: 5 }}>
+                    <Grid container alignItems="center" justifyContent="center" sx={{ mb: 5 }}>
                         <Grid item xs={12} md={8} order={{ xs: 2, md: 1 }} sx={{ pr: 10 }}>
-                            <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
+                            <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1, maxWidth: 450 }} >
                                 Getting corrections on your writing is really easy
                             </Typography>
                             <Typography variant="h6" paragraph>
@@ -76,14 +76,14 @@ const SectionTwo = () => {
                                 component="img"
                                 src="src/assets/index/illustration-1.webp"
                                 alt="illustration-1"
-                                sx={{ width: { xs: "50%", md: "80%" }, height: "auto" }}
+                                sx={{ width: { xs: "70%", md: "80%" }, height: "auto" }}
                             />
                         </Grid>
                     </Grid>
-                    <Grid container justifyContent="center">
+                    <Grid container columnSpacing={3} justifyContent="center">
                         {grids.map(grid => (
                             <Grid key={grid.id} item xs={12} md={4}>
-                                <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
+                                <Typography variant="h5" sx={{ fontWeight: "bold", mb: 1 }} >
                                     {grid.header}
                                 </Typography>
                                 <Typography variant="h6" paragraph>
@@ -139,10 +139,10 @@ const SectionThree = () => {
                     <Grid container justifyContent="center">
                         {grids.map(grid => (
                             <Grid key={grid.id} item xs={12} md={4}>
-                                <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
+                                <Typography variant="h5" sx={{ fontWeight: "bold", mb: 1 }} >
                                     {grid.header}
                                 </Typography>
-                                <Typography variant="h6" paragraph>
+                                <Typography variant="body1" paragraph>
                                     {grid.content}
                                 </Typography>
                             </Grid>
@@ -158,12 +158,12 @@ const SectionFour = () => (
     <Box component="section" py={6} bgcolor="primary.main" color="white">
         <BootstrapContainer>
             <Stack>
-                <Grid container justifyContent="center" sx={{ mb: 5 }}>
+                <Grid container justifyContent="center" alignItems="center" sx={{ mb: 5 }}>
                     <Grid item xs={12} md={8} sx={{ pr: 10 }}>
-                        <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }} >
+                        <Typography variant="h5" sx={{ fontWeight: "bold", mb: 1 }} >
                             Join today and experience language-learning, redefined.
                         </Typography>
-                        <Typography variant="h6" paragraph>
+                        <Typography variant="body1" paragraph>
                             Whether you’re fluent or just starting out, we’d be thrilled to have you join the LangCorrect community. We’re all learners and we understand that innd confidence in a new language, it’s important to make mistakes. LangCorrect’s wonderful users are ready to help you, provide support, and answer your burning questions so that you can reach the level you want to be at in your new language.
                         </Typography>
                         <Button color="inherit" variant="outlined">
