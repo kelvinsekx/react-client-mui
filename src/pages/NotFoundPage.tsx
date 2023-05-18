9924749868deb6b8ea1552345097852c2ab666d5
keyxcode <keyxcode@gmail.com>
2023-05-18 14:05:39 +0700

    Improve general stylings

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -2,11 +2,13 @@ import { Typography, Box, Grid, Button, Stack } from '@mui/material';
 import BootstrapContainer from "../components/BootstrapContainer"
 
 const NotFoundPage = () => (
-    <BootstrapContainer>
-        <Typography>Uh Oh.</Typography>
-        <Typography>Sorry, this page isn't available. The link you followed may be broken, or the page may have been removed.</Typography>
-        <Button>Back to Journals</Button>
-    </BootstrapContainer>
+    <Box component="section" sx={{ height: "80vh" }}>
+        <BootstrapContainer sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100%", textAlign: "center" }}>
+            <Typography variant="h3">Uh Oh.</Typography>
+            <Typography variant="body1" color="text.secondary">Sorry, this page isn't available. The link you followed may be broken, or the page may have been removed.</Typography>
+            <Button variant="contained">Back to Journals</Button>
+        </BootstrapContainer>
+    </Box>
 )
 
 export default NotFoundPage
