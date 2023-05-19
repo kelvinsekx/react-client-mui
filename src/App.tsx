1dd9f3de1eec6097e4d80c5881dcbed431ef6360
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-05-19 16:25:43 -0700

    Move providers outside of App and into main

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { CssBaseline } from '@mui/material';
-import { BrowserRouter } from 'react-router-dom';
-import RoutesList from './RoutesList.tsx';
-import { AuthContextProvider } from './context/AuthContext.tsx';
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
+import RoutesList from "./RoutesList.tsx";
 
 function App() {
     return (
         <>
-            <CssBaseline/>
-            <AuthContextProvider>
-                <BrowserRouter>
-                    <RoutesList/>
-                </BrowserRouter>
-            </AuthContextProvider>
+            <CssBaseline />
+            <BrowserRouter>
+                <RoutesList />
+            </BrowserRouter>
         </>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { AuthProvider } from "./context/AuthProvider.tsx";
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <AuthProvider>
+            <QueryClientProvider client={queryClient}>
+                <App />
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
+        </AuthProvider>
+    </React.StrictMode>
 );
